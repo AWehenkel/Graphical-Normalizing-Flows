0825637299bbb6f48f07b68d99e7003b5928b318
awehenkel <antoine.wehenkel@gmail.com>
2020-02-07 22:59:07 +0100

    Add loader for UCI

---
+++ LoadUCIExperiments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name "DAG-NF-UCI"
+#SBATCH --mem=6000M
+#SBATCH --time=20:00:00
+IN=$5
+arrIN=(${IN//,/ })
+python UCIExperiments.py -dataset $1 -nb_steps_dual $2 -max_l1 $3 -nb_epoch $4 -network ${arrIN[*]} -b_size $6 -load -folder $7
