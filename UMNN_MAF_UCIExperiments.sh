1c6d732153a53aee50f84d9ce04ed359ff2e33c3
awehenkel <antoine.wehenkel@gmail.com>
2020-02-10 14:54:29 +0100

    Add hidden embedding network

--- UMNN_MAF_UCIExperiments.sh
+++ UMNN_MAF_UCIExperiments.sh
@@ -8,5 +8,5 @@
 int_net=$4
 int_net=(${int_net//,/ })
 emb_net=$5
-emb_net=(${int_net//,/ })
+emb_net=(${emb_net//,/ })
 python UCIExperiments.py -dataset $1 -b_size $2 -nb_epoch $3 -int_net ${int_net[*]} -emb_net ${emb_net[*]} -UMNN_MAF
