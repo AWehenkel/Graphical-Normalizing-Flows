547748f5ddf264320024e4249f419dd3bb99513f
awehenkel <antoine.wehenkel@gmail.com>
2020-02-10 14:58:46 +0100

    Add hidden embedding network

--- models/MLP.py
+++ models/MLP.py
@@ -11,7 +11,7 @@ class MLP(nn.Module):
         self.device = device
         layers_dim = [in_d] + hidden + [out_d]
         layers = []
-        for dim_in, dim_out in zip(layers_dim[:-1], layers_dim[1]):
+        for dim_in, dim_out in zip(layers_dim[:-1], layers_dim[1:]):
             layers += [nn.Linear(dim_in, dim_out), act_f]
         self.net = nn.Sequential(*layers).to(device)
 
