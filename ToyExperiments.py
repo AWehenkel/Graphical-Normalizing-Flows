6251bdb1586fdb6ae9890ee1346a54163c34c0d9
awehenkel <antoine.wehenkel@gmail.com>
2020-02-12 14:39:33 +0100

    Make toys work with embedding net.

--- ToyExperiments.py
+++ ToyExperiments.py
@@ -25,8 +25,8 @@ def train_toy(toy, load=True, nb_step_dual=300, nb_steps=50, folder="", max_l1=1
     x = torch.tensor(toy_data.inf_train_gen(toy, batch_size=1000)).to(device)
 
     dim = x.shape[1]
-    emb_net = MLP(dim, hidden=[50, 50, 50], out_d=16, device=device)
-    model = DAGNF(in_d=dim, hidden_integrand=[100, 100, 100], emb_d=16, emb_net=emb_net, device=device,
+    emb_net = MLP(dim, hidden=[50, 50, 50], out_d=20, device=device)
+    model = DAGNF(in_d=dim, hidden_integrand=[100, 100, 100], emb_d=20, emb_net=emb_net, device=device,
                   l1_weight=.01, nb_steps=nb_steps)
 
     opt = torch.optim.Adam(model.parameters(), 1e-3, weight_decay=1e-5)
