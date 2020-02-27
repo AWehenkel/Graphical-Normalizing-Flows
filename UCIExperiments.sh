72a230461a8b542714d05d6f606188a1e2e8dab0
awehenkel <antoine.wehenkel@gmail.com>
2020-02-27 17:22:06 +0100

    Debug multiple steps (add multiple embedding nets)

--- UCIExperiments.sh
+++ UCIExperiments.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --job-name "DAG-NF-UCI"
 #SBATCH --mem=6000M
-#SBATCH --time=72:00:00
+#SBATCH --time=144:00:00
 #SBATCH --output="DAG-NF-UCI-%j.out"
 int_net=$6
 int_net=(${int_net//,/ })
