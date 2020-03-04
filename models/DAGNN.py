74ef40358ca145ae8cabaa7983183ac2c96c8342
awehenkel <antoine.wehenkel@gmail.com>
2020-03-04 15:57:37 +0100

    Add clamp for linear flow

--- models/DAGNN.py
+++ models/DAGNN.py
@@ -343,6 +343,7 @@ class DAGStep(nn.Module):
             elif self.dag_const > 0:
                 self.dag_embedding.get_dag().post_process(1e-1)
                 self.dag_const = 0.
+                self.l1_weight = 0.
         return lag_const
 
     def set_h_threshold(self, threshold):
