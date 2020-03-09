a3d212955f9954237bb1aebd7216227be5b2143b
awehenkel <antoine.wehenkel@gmail.com>
2020-03-09 16:26:19 +0100

    Improve constraint

--- UCIExperiments.py
+++ UCIExperiments.py
@@ -90,11 +90,11 @@ def train(dataset="POWER", load=True, nb_step_dual=100, nb_steps=20, path="", l1
         l1_weight = l1
         model = DAGNF(nb_flow=nb_flow, in_d=dim, hidden_integrand=int_net, emb_d=emb_nets[0].out_d, emb_nets=emb_nets, device=device,
                       l1_weight=l1, nb_steps=nb_steps, solver=solver, linear_normalizer=linear_net, gumble_T=gumble_T)
-        if nb_flow == 1:
-            model = DAGStep(in_d=dim, hidden_integrand=int_net, emb_d=emb_nets[0].out_d, emb_net=emb_nets[0],
-                            device=device, l1_weight=l1, nb_steps=nb_steps, solver=solver, linear_normalizer=linear_net,
-                            gumble_T=gumble_T)
-            model.nets = [model]
+        #if nb_flow == 1:
+        #    model = DAGStep(in_d=dim, hidden_integrand=int_net, emb_d=emb_nets[0].out_d, emb_net=emb_nets[0],
+        #                    device=device, l1_weight=l1, nb_steps=nb_steps, solver=solver, linear_normalizer=linear_net,
+        #                    gumble_T=gumble_T)
+        #    model.nets = [model]
     if min_pre_heating_epochs > 0:
         model.dag_const = 0.
     opt = torch.optim.Adam(model.parameters(), 1e-3, weight_decay=1e-5)
