746fa7c91cc59382986c25ceba5c3ed7eb8c20c2
awehenkel <antoine.wehenkel@gmail.com>
2020-04-21 16:04:47 +0200

    Some cleaning + one experiments for MNIST and CIFAR10.

---
+++ UCIExperimentsMultiple.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name "DAG-NF-UCI"
+#SBATCH --mem-per-cpu=2500M
+#SBATCH --time=144:00:00
+#SBATCH --output="DAG-NF-UCI-%j.out"
+
+args=("$@")
+supp_args=$(printf "%s "  "${args[@]}")
+
+source activate UMNN
+for config in "${args[@]}"
+do
+    srun python UCIExperiments.py $config
+done
